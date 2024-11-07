770f1a3cfe9aedb4ec43a370d3d021928ea443f5
imranpranto04 <imranh.pranto@gmail.com>
2024-11-07 22:23:33 +0600

    code reform

--- src/problem-4.ts
+++ src/problem-4.ts
@@ -15,26 +15,22 @@
 
   function calculateShapeArea(shape: Shape): number {
     if (shape.shape === "circle") {
-      // Calculate area of circle (π * r^2)
       return Math.PI * shape.radius ** 2;
     } else if (shape.shape === "rectangle") {
-      // Calculate area of rectangle (width * height)
       return shape.width * shape.height;
     }
-    return 0; // Fallback (should never be reached)
+    return 0;
   }
 
-  // Sample Input 1
   const circleArea: number = parseFloat(
     calculateShapeArea({ shape: "circle", radius: 5 }).toFixed(2)
   );
-  console.log(circleArea); // Output: 78.54
+  console.log(circleArea);
 
-  // Sample Input 2
   const rectangleArea: number = calculateShapeArea({
     shape: "rectangle",
     width: 4,
     height: 6,
   });
-  console.log(rectangleArea); // Output: 24
+  console.log(rectangleArea);
 }
